a74cdd7c33bab185c64e9ebf48616967fadb2685
NickKaparinos <nickkaparinos@gmail.com>
2021-10-02 00:57:15 +0300

    Updated Reacher

--- MuJoCo/Reacher/main.py
+++ MuJoCo/Reacher/main.py
@@ -5,14 +5,16 @@ Nick Kaparinos
 """
 
 import pickle
+
 import gym
 import tianshou as ts
 from tianshou.exploration import GaussianNoise
-from tianshou.policy import DDPGPolicy
+from tianshou.policy import SACPolicy
 from tianshou.utils import TensorboardLogger
 from tianshou.utils.net.common import Net
-from tianshou.utils.net.continuous import Actor, Critic
+from tianshou.utils.net.continuous import Critic, ActorProb
 from torch.utils.tensorboard import SummaryWriter
+
 from utilities import *
 
 if __name__ == '__main__':
@@ -26,7 +28,7 @@ if __name__ == '__main__':
     model_log_dir, model_file, buffer_file = '', '', ''
 
     # Logging directory
-    model_name = 'Tianshou_DDPG'
+    model_name = 'Tianshou_SAC'
     log_dir = 'logs/' + model_name + '_' + str(time.strftime('%d_%b_%Y_%H_%M_%S', time.localtime())) + '/'
     writer = SummaryWriter(log_dir=log_dir)
     logger = TensorboardLogger(writer, train_interval=1, update_interval=1)
@@ -34,17 +36,15 @@ if __name__ == '__main__':
     # Environment
     env = gym.make(env_id)
     env.seed(seed=seed)
+    num_envs = 5
     max_episode_steps = 250
-
-
     def make_env():
         env = gym.make(env_id)
         env._max_episode_steps = max_episode_steps
         return env
 
-
-    train_envs = ts.env.DummyVectorEnv([lambda: make_env() for _ in range(1)])
-    test_envs = ts.env.DummyVectorEnv([lambda: make_env() for _ in range(1)])
+    train_envs = ts.env.DummyVectorEnv([lambda: make_env() for _ in range(num_envs)])
+    test_envs = ts.env.DummyVectorEnv([lambda: make_env() for _ in range(num_envs)])
     train_envs.seed(seed)
     test_envs.seed(seed)
 
@@ -54,17 +54,32 @@ if __name__ == '__main__':
     max_action = env.action_space.high[0]
     model_hyperparameters = {'hidden_sizes': [128, 128], 'learning_rate': 1e-3, 'estimation_step': 1}
 
+    # Actor
     net_a = Net(state_shape, hidden_sizes=model_hyperparameters['hidden_sizes'], device=device)
-    actor = Actor(net_a, action_shape, max_action=max_action, device=device).to(device)
+    actor = ActorProb(net_a, action_shape, max_action=max_action, device=device, unbounded=True).to(device)
     actor_optim = torch.optim.Adam(actor.parameters(), lr=model_hyperparameters['learning_rate'])
-    net_c = Net(state_shape, action_shape, hidden_sizes=model_hyperparameters['hidden_sizes'], concat=True,
-                device=device)
-    critic = Critic(net_c, device=device).to(device)
-    critic_optim = torch.optim.Adam(critic.parameters(), lr=model_hyperparameters['learning_rate'])
-    policy = DDPGPolicy(actor, actor_optim, critic, critic_optim,
-                        exploration_noise=GaussianNoise(sigma=0.5 * max_action),
-                        estimation_step=model_hyperparameters['estimation_step'],
-                        action_space=env.action_space)
+
+    # Critics
+    net_c1 = Net(state_shape, action_shape, hidden_sizes=model_hyperparameters['hidden_sizes'], concat=True,
+                 device=device)
+    critic1 = Critic(net_c1, device=device).to(device)
+    critic1_optim = torch.optim.Adam(critic1.parameters(), lr=model_hyperparameters['learning_rate'])
+    net_c2 = Net(state_shape, action_shape, hidden_sizes=model_hyperparameters['hidden_sizes'], concat=True,
+                 device=device)
+    critic2 = Critic(net_c2, device=device).to(device)
+    critic2_optim = torch.optim.Adam(critic2.parameters(), lr=model_hyperparameters['learning_rate'])
+
+    # Alpha
+    target_entropy = -np.prod(env.action_space.shape)
+    log_alpha = torch.zeros(1, requires_grad=True, device=device)
+    alpha_lr = 1e-4
+    alpha_optim = torch.optim.Adam([log_alpha], lr=alpha_lr)
+    alpha = (target_entropy, log_alpha, alpha_optim)
+
+    policy = SACPolicy(actor, actor_optim, critic1, critic1_optim, critic2, critic2_optim,
+                       exploration_noise=GaussianNoise(sigma=0.5 * max_action),
+                       estimation_step=model_hyperparameters['estimation_step'],
+                       action_space=env.action_space, alpha=alpha)
 
     # Collectors
     use_prioritised_replay_buffer = False
@@ -75,7 +90,7 @@ if __name__ == '__main__':
                                             exploration_noise=True)
     else:
         train_collector = ts.data.Collector(policy, train_envs,
-                                            ts.data.ReplayBuffer(size=prioritized_buffer_hyperparameters['total_size']),
+                                            ts.data.VectorReplayBuffer(total_size=prioritized_buffer_hyperparameters['total_size']*num_envs, buffer_num=num_envs),
                                             exploration_noise=True)
     test_collector = ts.data.Collector(policy, test_envs, exploration_noise=False)
 
@@ -86,6 +101,7 @@ if __name__ == '__main__':
 
         def custom_sigma_schedule(epoch, env_step):
             step_number = (epoch - 1) * steps_per_epoch + env_step
+
             current_sigma = max_sigma - step_number * decay_per_step
             if current_sigma < 0.0:
                 current_sigma = 0.0
@@ -101,13 +117,15 @@ if __name__ == '__main__':
 
             # Save agent
             torch.save({'model': policy.state_dict(), 'actor_optim': actor_optim.state_dict(),
-                        'critic_optim': critic_optim.state_dict()},
+                        'critic1_optim': critic1_optim.state_dict(), 'critic2_optim': critic2_optim.state_dict()},
                        log_dir + model_name + f'_epoch{epoch}.pth')
             pickle.dump(train_collector.buffer, open(log_dir + f'epoch{epoch}_train_buffer.pkl', "wb"))
 
-            # Record agents performance in video with and without exploration
+            # Record agents performance in video
             for episode in range(num_episodes // 2):
                 env = ts.env.DummyVectorEnv([lambda: make_env() for _ in range(1)])
+
+                # Video
                 policy.eval()
                 collector = ts.data.Collector(policy, env, exploration_noise=True)
                 collector.collect_and_record = collect_and_record
@@ -116,6 +134,8 @@ if __name__ == '__main__':
 
             for episode in range(num_episodes // 2, num_episodes):
                 env = ts.env.DummyVectorEnv([lambda: make_env() for _ in range(1)])
+
+                # Video
                 policy.eval()
                 collector = ts.data.Collector(policy, env, exploration_noise=False)
                 collector.collect_and_record = collect_and_record
@@ -126,44 +146,46 @@ if __name__ == '__main__':
 
 
     # Training
-    trainer_hyperparameters = {'max_epoch': 5, 'step_per_epoch': 75_000, 'step_per_collect': 100,
-                               'episode_per_test': 100, 'batch_size': 128}
-    decay_steps = int(trainer_hyperparameters['max_epoch'] * trainer_hyperparameters['step_per_epoch'] * 1)
-    build_sigma_hyperparameters = {'max_sigma': 1, 'min_sigma': 0.1, 'decay_time_steps': decay_steps}
-    all_hypeparameters = dict(model_hyperparameters, **prioritized_buffer_hyperparameters, **trainer_hyperparameters,
+    trainer_hyperparameters = {'max_epoch': 4, 'step_per_epoch': 250_000, 'step_per_collect': 1,
+                               'episode_per_test': 10, 'batch_size': 64}
+    decay_steps = int(trainer_hyperparameters['max_epoch'] * trainer_hyperparameters['step_per_epoch'] * 0.001)
+    build_sigma_hyperparameters = {'max_sigma': 0.5, 'min_sigma': 0.0, 'decay_time_steps': decay_steps}
+    all_hypeparameters = dict(model_hyperparameters, **trainer_hyperparameters, **prioritized_buffer_hyperparameters,
                               **build_sigma_hyperparameters)
     all_hypeparameters['seed'] = seed
+    all_hypeparameters['use_prioritised_replay_buffer'] = use_prioritised_replay_buffer
+    all_hypeparameters['alpha_lr'] = alpha_lr
+    all_hypeparameters['num_envs'] = num_envs
     if load_pretrained_model:
         # Load model, optimisers and buffer
         checkpoint = torch.load(model_log_dir + model_file)
         policy.load_state_dict(checkpoint['model'])
         policy.actor_optim.load_state_dict(checkpoint['actor_optim'])
-        policy.critic_optim.load_state_dict(checkpoint['critic_optim'])
+        policy.critic1_optim.load_state_dict(checkpoint['critic1_optim'])
+        policy.critic2_optim.load_state_dict(checkpoint['critic2_optim'])
         train_collector.buffer = pickle.load(open(model_log_dir + buffer_file, "rb"))
         all_hypeparameters['load_pretrained_model'] = load_pretrained_model
         all_hypeparameters['model_log_dir'] = model_log_dir
         all_hypeparameters['model_file'] = model_file
         all_hypeparameters['buffer_file'] = buffer_file
-
-    all_hypeparameters['decay_steps'] = decay_steps
     save_dict_to_file(all_hypeparameters, path=log_dir)
 
     result = ts.trainer.offpolicy_trainer(policy, train_collector, test_collector, **trainer_hyperparameters,
                                           train_fn=build_sigma_schedule(**build_sigma_hyperparameters,
                                                                         steps_per_epoch=trainer_hyperparameters[
                                                                             'step_per_epoch']),
-                                          test_fn=build_test_fn(num_episodes=8), stop_fn=None, logger=logger)
+                                          test_fn=build_test_fn(num_episodes=8), stop_fn=None,
+                                          logger=logger)
     print(f'Finished training! Use {result["duration"]}')
 
     # Learning Curve
-    learning_curve_tianshou(log_dir=log_dir, window=25)
+    learning_curve_tianshou(log_dir=log_dir, window=50)
 
     # Record Episode Video
     num_episodes = 10
     for episode in range(num_episodes):
         # Video
         env = ts.env.DummyVectorEnv([lambda: make_env() for _ in range(1)])
-        env.spec[0].max_episode_steps = max_episode_steps
         policy.eval()
         collector = ts.data.Collector(policy, env, exploration_noise=False)
         collector.collect_and_record = collect_and_record
