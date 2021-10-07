0e23a51fbcd40333975d7cf8f03fdd6ec8d606ca
NickKaparinos <nickkaparinos@gmail.com>
2021-10-07 21:07:19 +0300

    Added BipedalHardcore, HalfCheetah and InvertedDoublePendulum

---
+++ Box2D/BipedalWalkerHardcore/utilities.py
@@ -0,0 +1,191 @@
+"""
+Open AI Gym BipedalWalkerHardcore-v3
+Nick Kaparinos
+2021
+"""
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from stable_baselines3.common.callbacks import BaseCallback
+from tqdm import tqdm
+import gym
+import os
+from os import listdir
+from tensorflow.python.summary.summary_iterator import summary_iterator
+
+
+class LogStepsCallback(BaseCallback):
+    def __init__(self, log_dir, last_epoch=False, verbose=0):
+        self.log_dir = log_dir
+        self.results = pd.DataFrame(columns=['Reward', 'Done'])
+        self.last_epoch = last_epoch
+        super(LogStepsCallback, self).__init__(verbose)
+
+    def _on_training_start(self) -> None:
+        print("Τraining starts!")
+
+    def _on_step(self) -> bool:
+        if 'reward' in self.locals:
+            keys = ['reward', 'done']
+        else:
+            keys = ['rewards', 'dones']
+        self.results.loc[len(self.results)] = [self.locals[keys[0]][0], self.locals[keys[1]][0]]
+        return True
+
+    def _on_training_end(self) -> None:
+        if self.last_epoch:
+            self.results.to_csv(self.log_dir + 'training_data.csv', index=False)
+        else:
+            if os.path.exists(self.log_dir + 'training_data_temp.csv'):
+                os.remove(self.log_dir + 'training_data_temp.csv')
+            self.results.to_csv(self.log_dir + 'training_data_temp.csv', index=False)
+        print("Τraining ends!")
+
+
+class TqdmCallback(BaseCallback):
+    def __init__(self, epoch_timesteps):
+        super().__init__()
+        self.progress_bar = None
+        self.epoch_timesteps = epoch_timesteps
+
+    def _on_training_start(self):
+        self.progress_bar = tqdm(total=self.epoch_timesteps)
+
+    def _on_step(self):
+        self.progress_bar.update(1)
+        return True
+
+    def _on_training_end(self):
+        self.progress_bar.close()
+        self.progress_bar = None
+
+
+def save_dict_to_file(dict, path, txt_name='hyperparameter_dict'):
+    f = open(path + '/' + txt_name + '.txt', 'w')
+    f.write(str(dict))
+    f.close()
+
+
+def calc_episode_rewards(training_data):
+    # Calculate the rewards for each training episode
+    episode_rewards = []
+    temp_reward_sum = 0
+
+    for step in range(training_data.shape[0]):
+        reward, done = training_data.iloc[step, :]
+        temp_reward_sum += reward
+        if done:
+            episode_rewards.append(temp_reward_sum)
+            temp_reward_sum = 0
+
+    result = pd.DataFrame(columns=['Reward'])
+    result['Reward'] = episode_rewards
+    return result
+
+
+def learning_curve(episode_rewards, log_dir, window=10):
+    # Calculate rolling window metrics
+    rolling_average = episode_rewards.rolling(window=window, min_periods=window).mean().dropna()
+    rolling_max = episode_rewards.rolling(window=window, min_periods=window).max().dropna()
+    rolling_min = episode_rewards.rolling(window=window, min_periods=window).min().dropna()
+
+    # Change column name
+    rolling_average.columns = ['Average Reward']
+    rolling_max.columns = ['Max Reward']
+    rolling_min.columns = ['Min Reward']
+    rolling_data = pd.concat([rolling_average, rolling_max, rolling_min], axis=1)
+
+    # Plot
+    sns.set()
+    ax = sns.lineplot(data=rolling_data)
+    ax.fill_between(rolling_average.index, rolling_min.iloc[:, 0], rolling_max.iloc[:, 0], alpha=0.2)
+    ax.set_title('Learning Curve')
+    ax.set_ylabel('Reward')
+    ax.set_xlabel('Episodes')
+    plt.ylim((-100, 325))
+
+    # Save figure
+    plt.savefig(log_dir + 'learning_curve' + str(window) + '.png')
+
+
+def learning_curve_baselines(log_dir, window=10):
+    # Read data
+    training_data = pd.read_csv(log_dir + 'training_data.csv', index_col=None)
+
+    # Calculate episode rewards
+    episode_rewards = calc_episode_rewards(training_data)
+
+    learning_curve(episode_rewards=episode_rewards, log_dir=log_dir, window=window)
+
+
+def learning_curve_tianshou(log_dir, window=10):
+    # Find event file
+    files = listdir(log_dir)
+    for f in files:
+        if 'events' in f:
+            event_file = f
+            break
+
+    # Read episode rewards
+    episode_rewards_list = []
+    episode_rewards = pd.DataFrame(columns=['Reward'])
+    try:
+        for e in summary_iterator(log_dir + event_file):
+            if len(e.summary.value) > 0:
+                if e.summary.value[0].tag == 'train/reward':
+                    episode_rewards_list.append(e.summary.value[0].simple_value)
+    except Exception as e:
+        pass
+    episode_rewards['Reward'] = episode_rewards_list
+
+    # Learning curve
+    learning_curve(episode_rewards, log_dir, window=window)
+
+
+def learning_curve_tianshou_multiple_runs(log_dirs, window=10):
+    episode_rewards_list = []
+    episode_rewards = pd.DataFrame(columns=['Reward'])
+
+    for log_dir in log_dirs:
+        # Find event file
+        files = listdir(log_dir)
+        for f in files:
+            if 'events' in f:
+                event_file = f
+                break
+
+        # Read episode rewards
+        try:
+            for e in summary_iterator(log_dir + event_file):
+                if len(e.summary.value) > 0:
+                    if e.summary.value[0].tag == 'train/reward':
+                        episode_rewards_list.append(e.summary.value[0].simple_value)
+        except Exception as e:
+            pass
+    episode_rewards['Reward'] = episode_rewards_list
+
+    # Learning curve
+    learning_curve(episode_rewards, log_dir, window=window)
+
+
+class Wrapper(gym.Wrapper):
+    """Env wrapper for reward scale, action repeat and removing done penalty"""
+
+    def __init__(self, env, action_repeat=3, reward_scale=5, rm_done=True):
+        super().__init__(env)
+        self.action_repeat = action_repeat
+        self.reward_scale = reward_scale
+        self.rm_done = rm_done
+
+    def step(self, action):
+        r = 0.0
+        for _ in range(self.action_repeat):
+            obs, reward, done, info = self.env.step(action)
+            # remove done reward penalty
+            if not done or not self.rm_done:
+                r = r + reward
+            if done:
+                break
+        # scale reward
+        return obs, self.reward_scale * r, done, info
